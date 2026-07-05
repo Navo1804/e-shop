6a8dab0a44a296dbfbc40bb47d5410a02cd2f87c
Angelo Navarrete <anavarretev2@est.ups.edu.ec>
2026-07-05 00:04:49 -0500

    Configuracion clase Modelos

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,32 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
 from app.config import Config
-
 
 db = SQLAlchemy()
 migrate = Migrate()
 
-
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from app.models.usuario import Usuario
-    from app.models.producto import Producto
-    from app.models.pedido import Pedido
+    #Modelos
+    from app.models import Usuario
+    from app.models import Categoria
+    from app.models import Producto        
+    from app.models import Pedido, DetallePedido  
 
-    from app.blueprints.public.routes import public_bp
-    from app.blueprints.auth.routes import auth_bp
-    from app.blueprints.admin.routes import admin_bp
+
+    # Blueprints
+    from app.blueprints.public import public_bp
+    from app.blueprints.auth import auth_bp
+    from app.blueprints.admin import admin_bp
 
     app.register_blueprint(public_bp)
-    app.register_blueprint(auth_bp, url_prefix="/auth")
-    app.register_blueprint(admin_bp, url_prefix="/admin")
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(admin_bp, url_prefix='/admin')
 
     return app
